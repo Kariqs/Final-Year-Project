05052c54a7187538c1e66e6ea0996a231aecda8b
Kariqs <kariukibenard189@gmail.com>
2023-01-27 20:30:09 +0300

    eHealth

--- app/src/main/java/com/example/ehealth/Home.java
+++ app/src/main/java/com/example/ehealth/Home.java
@@ -1,24 +1,42 @@
 package com.example.ehealth;
 
+import static java.time.LocalTime.now;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.time.LocalTime;
 
 public class Home extends AppCompatActivity {
+    TextView Greetings;
 CardView Fasting,Training,Dieting,Results;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Greetings = findViewById(R.id.greetings);
         Fasting = findViewById(R.id.fast);
         Training = findViewById(R.id.workout);
         Dieting = findViewById(R.id.diet);
         Results = findViewById(R.id.results);
+
+        if (now().isBefore(LocalTime.of(12,00))){
+            Greetings.setText("Good Morning, Welcome.");
+        }else if (now().isAfter(LocalTime.of(12,00)) && now().isBefore(LocalTime.of(16,00))){
+            Greetings.setText("Good Afternoon, Welcome");
+        }else if (now().isAfter(LocalTime.of(16,00))){
+            Greetings.setText("Good Evening, Welcome.");
+        }
 
         Fasting.setOnClickListener(new View.OnClickListener() {
             @Override

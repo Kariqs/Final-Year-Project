fe78e6ec18ba292cbcb47965b7e51bb024ab1d31
Kariqs <kariukibenard189@gmail.com>
2023-02-05 19:07:16 +0300

    eHealth

--- app/src/main/java/com/example/ehealth/Fast.java
+++ app/src/main/java/com/example/ehealth/Fast.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 
 public class Fast extends AppCompatActivity {
-    TextView Countdown;
+    ProgressBar Progress;
+    TextView Countdown, Explain;
     Button StartEnd;
+    EditText EnterHours;
     CardView SixteenEight, Omad, TwentyFour, Autophagy;
 
     CountDownTimer countDownTimer;
@@ -26,6 +30,9 @@ public class Fast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fast);
+
+        EnterHours = findViewById(R.id.enterHours);
+        Explain = findViewById(R.id.explain);
         Countdown = findViewById(R.id.countDownTime);
         StartEnd = findViewById(R.id.startEnd);
         SixteenEight = findViewById(R.id.sixteenEight);
@@ -36,26 +43,27 @@ public class Fast extends AppCompatActivity {
         StartEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             countDownTimer.start();
-             StartEnd.setText("END FAST");
+                Explain.setText("YOUR FAST HAS STARTED, GOOD LUCK.");
+                countDownTimer.start();
+                StartEnd.setText("END FAST");
             }
         });
         SixteenEight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Countdown.setText("16:00:00");
-                TimeLeftInMillis =  57600000;
-                countDownTimer = new CountDownTimer(TimeLeftInMillis,1000) {
+                TimeLeftInMillis = 57600000;
+                countDownTimer = new CountDownTimer(TimeLeftInMillis, 1000) {
                     @Override
                     public void onTick(long l) {
-                    TimeLeftInMillis = l;
-                    updateCountDownText();
+                        TimeLeftInMillis = l;
+                        updateCountDownText();
                     }
 
                     @Override
                     public void onFinish() {
-                     TimerRunning = false;
-                     StartEnd.setVisibility(View.INVISIBLE );
+                        TimerRunning = false;
+                        StartEnd.setVisibility(View.INVISIBLE);
                     }
                 };
                 TimerRunning = true;
@@ -63,6 +71,7 @@ public class Fast extends AppCompatActivity {
                 //StartEnd.setVisibility(View.VISIBLE);
             }
         });
+
     }
 
     public void updateCountDownText() {
@@ -73,7 +82,7 @@ public class Fast extends AppCompatActivity {
         minutes = minutes % 60;
         seconds = seconds % 60;
 
-        String timeLeftFormated = String.format(Locale.getDefault(), "%02d:%02d:%02d",hours, minutes, seconds);
+        String timeLeftFormated = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
         Countdown.setText(timeLeftFormated);
 
 

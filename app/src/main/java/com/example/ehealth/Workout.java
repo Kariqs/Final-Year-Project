33766e3403401121c9fbccd390457c58c0c2dcb9
Kariqs <kariukibenard189@gmail.com>
2023-02-15 13:11:15 +0300

    eHealth

--- app/src/main/java/com/example/ehealth/Workout.java
+++ app/src/main/java/com/example/ehealth/Workout.java
@@ -3,15 +3,17 @@ package com.example.ehealth;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Workout extends AppCompatActivity {
 
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
-
+        recyclerView = findViewById(R.id.recyclerView);
     }
 
 

485b4b85654b4d84720cd970872edd7d16dff8fb
kariuki <kariukibenard189@gmail.com>
2023-02-23 13:37:35 +0300

    eHealth

--- app/src/main/java/com/example/ehealth/UploadResults.java
+++ app/src/main/java/com/example/ehealth/UploadResults.java
@@ -1,13 +1,17 @@
 package com.example.ehealth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
 
 import java.net.URI;
 
@@ -18,7 +22,7 @@ public class UploadResults extends AppCompatActivity {
     private ImageView Display;
     private ProgressBar uploadProgress;
 
-    private URI uri;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +33,28 @@ public class UploadResults extends AppCompatActivity {
         Description = findViewById(R.id.caption);
         Display = findViewById(R.id.display);
         uploadProgress = findViewById(R.id.uploadProgress);
+
+        pickImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+
+    }
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data !=null && data.getData()!=null){
+            uri = data.getData();
+            Picasso.get().load(uri).into(Display);
+        }
     }
 }
